8f317fb476b0de66f0a1513b5f778138adca3392
brian <smithbas1994@gmail.com>
2017-12-03 18:51:27 -0800

    - completed setup.js - started working on server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
-var netmask = require('netmask').Netmask;
-var ip = require('ip');
+var ifconfig = require('wireless-tools/ifconfig');
+var setup = require('./setup.js');
 
-var block = new netmask(ip.address() + '/24');
-var BROADCAST_ADDR = block.broadcast;
-var PORT = 6024;
+var connected_networks = [];
 
+//setup access point and connect to mesh network
+setup.run().then(function (data) {
+  connected_networks.concat(data);
+  console.log(data);
+}).catch(function (error) {
+  console.log(error);
+});
+
+
+ifconfig.status(function(err, status) {
+  console.log(status);
+});
+
+/*
 server.bind(function() {
   server.setBroadcast(true);
   setInterval(function() {
@@ -16,7 +28,8 @@ server.bind(function() {
 
 function broadcastMessage(data) {
   var message = new Buffer(data);
-  server.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
+  server.send(message, 0, message.length, 6024, BROADCAST_ADDR, function() {
     console.log("Sent '" + message + "'");
   });
 }
+*/
--- setup.js
+++ setup.js
@@ -3,49 +3,64 @@ var _ = require('lodash');
 var ifconfig = require('wireless-tools/ifconfig');
 var hostapd = require('wireless-tools/hostapd');
 var iwlist = require('wireless-tools/iwlist');
+var wpa_supplicant = require('wireless-tools/wpa_supplicant');
+
+var setup = {};
 
 
-
-q.Promise(function (resolve, reject, notify) {
-  //get list of interfaces
-  ifconfig.status(function(error, status) {
-    if(error) reject(error);
-    resolve(_.slice(status, _.findIndex(status, {interface: 'lo'})+1));
-  });
-}).then(function (network_interfaces) {
-  console.log(network_interfaces);
-
-  //setup access point
+setup.run = function (query) {
   return q.Promise(function (resolve, reject, notify) {
-    hostapd.enable({
-      channel: Math.floor(Math.random()*(10-1+1)+1),
-      hw_mode: 'g',
-      interface: network_interfaces[0].interface,
-      ssid: 'meshnet_'+network_interfaces[0].address
-      //wpa: 2,
-      //wpa_passphrase: 'password'
-    }, function(error) {
+    //get list of interfaces
+    ifconfig.status(function(error, status) {
       if(error) reject(error);
-      resolve();
+      resolve(_.slice(status, _.findIndex(status, {interface: 'lo'})+1));
     });
-  }).then(function (data) {
-    //scan for mesh connections
-    var connectors = _.drop(network_interfaces);
+  }).then(function (network_interfaces) {
+
+    //setup access point
     return q.Promise(function (resolve, reject, notify) {
-      iwlist.scan(connectors[0].interface, function(error, networks) {
+      hostapd.enable({
+        channel: Math.floor(Math.random()*(10-1+1)+1),
+        hw_mode: 'g',
+        interface: network_interfaces[0].interface,
+        ssid: 'meshnet_'+network_interfaces[0].address
+      }, function(error) {
         if(error) reject(error);
-        resolve(_.filter(networks, function(o) {
-          return _.includes(o.ssid, 'meshnet_');
-        }));
+        resolve();
       });
     }).then(function (data) {
-      if(data.length < 1) throw 'No meshnet networks found';
-      //connect to mesh connections
-      return q.all(connectors.map(function (curr, index) {
 
-      }));
+      //scan for mesh connections
+      var connectors = _.drop(network_interfaces);
+      if(connectors.length === 0) throw 'No external network cards found';
+      return q.Promise(function (resolve, reject, notify) {
+        iwlist.scan(connectors[0].interface, function(error, networks) {
+          if(error) reject(error);
+          resolve(_.filter(networks, function(o) {
+            return _.includes(o.ssid, 'meshnet_') && o.ssid !== 'meshnet_'+network_interfaces[0].address;
+          }));
+        });
+      }).then(function (data) {
+
+        //connect to mesh connections
+        if(data.length === 0) throw 'No meshnet networks found';
+        return q.all(connectors.map(function (curr, index) {
+          return q.Promise(function (resolve, reject, notify) {
+            wpa_supplicant.enable({
+              interface: curr.interface,
+              ssid: data[index].ssid,
+              driver: 'wext'
+            }, function(error) {
+              if(error) reject(error);
+              resolve(data[index].ssid);
+            });
+          });
+        }));
+
+      });
     });
   });
-}).catch(function (error) {
-  console.log('ERROR: ' + error);
-});
+};
+
+
+module.exports = setup;
