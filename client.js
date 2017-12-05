d5c38a2afa198ebaa53e10bfb51ea7d5053c65d7
brian <smithbas1994@gmail.com>
2017-12-04 22:00:05 -0800

    - added listening to relays on client.js

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 var dgram = require('dgram');
 var client = dgram.createSocket("udp4");
+var relay = dgram.createSocket("udp4");
 var inquirer = require('inquirer');
 
 var BROADCAST_ADDR = '255.255.255.255';
 var LOCAL_PORT = 6024;
+var RELAY_PORT = 6025;
 var name = '';
 
 //Listen to broadcast messages from other users
@@ -21,9 +23,29 @@ client.on('message', function(message, info){
   }
 });
 
+//Listen to broadcast messages from relays
+relay.on('message', function(message, info){
+  try{
+    //parse JSON string
+    var parsed = JSON.parse(message);
+
+    if(name !== parsed.name){
+      //log user message to terminal
+      console.log(parsed.name + ': ' + parsed.message);
+    }
+  }catch(e) {
+    //IGNORE: failed to parse message
+  }
+});
+
 //bind port to listen for new broadcast messages from clients
 client.bind(LOCAL_PORT, function() {
   client.setBroadcast(true);
+});
+
+//bind port to listen for new broadcast messages from relays
+relay.bind(RELAY_PORT, function() {
+  relay.setBroadcast(true);
 });
 
 //get name from user
